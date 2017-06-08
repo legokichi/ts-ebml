e1a4a89cec2113876efdabeb3b49763bdb664bb3
legokichi <legokichi@gmail.com>
2017-06-08 16:15:08 +0900

    add cli tool

---
+++ src/cli.ts
@@ -0,0 +1,44 @@
+import {Decoder, tools} from './';
+import com = require('commander');
+import fs = require('fs');
+const version: string = require("../package.json").version;
+
+com
+  .version(version)
+  //.option('-p, --peppers', 'Add peppers')
+  //.option('-P, --pineapple', 'Add pineapple')
+  //.option('-b, --bbq-sauce', 'Add bbq sauce')
+  //.option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .arguments('<*.webm>')
+  .parse(process.argv);
+
+const {args} = com;
+
+if(args.length < 1){ process.exit(); }
+
+const decoder = new Decoder();
+
+fs.createReadStream(args[0]).on('data', (buf)=>{
+  const ebmlElms = decoder.decode(buf);
+  ebmlElms.forEach((elm)=>{
+    const {name, type, tagStart} = elm;
+    if(elm.type === "m"){
+      if(!elm.isEnd){
+        console.log(`${tagStart}\t${type}\t${name}`);
+      }
+    }else{
+      if(elm.type === "b"){
+        if(elm.name === "SimpleBlock"){
+          const {discardable, frames, invisible, keyframe, timecode, trackNumber} = tools.ebmlBlock(elm.value);
+          console.log(`${tagStart}\t${type}\t${name}`, `track:${trackNumber} timecode:${timecode}\tkeyframe:${keyframe}\tinvisible:${invisible}\tdiscardable:${discardable}\tlacying:${frames.length}`);
+        }else{
+          console.log(`${tagStart}\t${type}\t${name}`, `<Buffer ${elm.value.byteLength}>`);
+        }
+      }else if(elm.type === "d"){
+        console.log(`${tagStart}\t${type}\t${name}`, tools.convertEBMLDateToJSDate(elm.value));
+      }else{
+        console.log(`${tagStart}\t${type}\t${name}`, elm.value);
+      }
+    }
+  });
+});
