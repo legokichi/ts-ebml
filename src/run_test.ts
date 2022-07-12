bd61943dbeb5af6f47823688a92bab43fcb95a0b
David Halls <dahalls@gmail.com>
2022-07-12 06:50:31 +0100

    Test: Log on fail

--- src/run_test.ts
+++ src/run_test.ts
@@ -1,3 +1,4 @@
+import { EOL } from 'os';
 import { test, expect } from '@playwright/test';
 
 const expected_num_tests = 17;
@@ -15,4 +16,23 @@ test('tests should pass', async ({ page }) => {
         skipped: 0,
         todo: 0
     });
+});
+
+let log_on_fail;
+
+test.beforeEach(async ({ page }) => {
+    log_on_fail = '';
+    page.on('console', msg => {
+        //console.log(msg.text());
+        log_on_fail += msg.text() + EOL;
+    });
+    page.on('pageerror', err => {
+        log_on_fail += err.message + EOL;
+    });
+});
+
+test.afterEach(async ({}, testInfo) => {
+    if (testInfo.status !== testInfo.expectedStatus) {
+        console.error(log_on_fail);
+    }
 });
